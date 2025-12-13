ba071ecc3db79e818de81e26d94837e3faf0fb92
coder371 <abdalstar371371@gmail.com>
2025-12-14 00:01:55 +0200

    Add welcome widget and update styles for improved layout and functionality

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Main JS loaded');
+    const getStartedButton = document.getElementById('get-started');
+    getStartedButton.addEventListener('click', () => {
+        window.open('https://docs.qumra.cloud/', '_blank');
+    });
 });
